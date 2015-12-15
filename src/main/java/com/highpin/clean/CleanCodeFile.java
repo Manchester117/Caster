33a04551cd37ef8e1bfcf0990aa54a3858a01604
ZhaoPeng <zhao.peng@zhaopin.com.cn>
2015-12-15 23:02:34 +0800

    增加清理报告和截图

--- src/main/java/com/highpin/clean/CleanCodeFile.java
+++ src/main/java/com/highpin/clean/CleanCodeFile.java
@@ -2,12 +2,33 @@ package com.highpin.clean;
 
 import com.highpin.tools.Utility;
 
+import java.io.File;
+
 /**
  * Created by Administrator on 2015/12/3.
  */
 public class CleanCodeFile {
+    public void cleanReportAndScreenShot() {
+        String reportPath = "report";
+        String screenShotPath = "screenshot";
+
+        this.deleteFiles(reportPath);
+        this.deleteFiles(screenShotPath);
+    }
+
+    private void deleteFiles(String path) {
+        File folder = new File(path);
+        File [] fileList = folder.listFiles();
+        if (fileList != null) {
+            for (File file: fileList) {
+                file.delete();
+            }
+        }
+    }
     // 删除所有代码文件
     public static void main(String[] args) {
+        CleanCodeFile ccf = new CleanCodeFile();
+        ccf.cleanReportAndScreenShot();
         Utility.cleanCodeFile();
     }
 }
